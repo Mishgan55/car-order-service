0b6098502a2a120d997c5d600f47abe790338270
mishgan55 <vorota.mogilev@gmail.com>
2023-10-12 11:51:22 +0300

    Initial commit

---
+++ src/main/java/innowise/khorsun/carorderservice/CarOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package innowise.khorsun.carorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarOrderServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/innowise/khorsun/carorderservice/CarOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package innowise.khorsun.carorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
